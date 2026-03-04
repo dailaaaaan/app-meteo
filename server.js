7fee7745bb217b0cca1210616ec6dc4fd9d8c462
dailaaaaan <dylanandreufiallos@gmail.com>
2026-03-04 18:43:26 +0100

    PASO 7- Nuevo server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { MongoClient} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 app.use(express.json());
@@ -11,22 +11,47 @@ const client = new MongoClient(process.env.MONGO_URI);
 async function connectarDB() {
     try {
         await client.connect();
-        console.log("Connectat a MongoDB Atlas");
 
         const db = client.db('meteoDB');
         const alertesCol = db.collection('alertes');
 
+        console.log("Connectat a MongoDB Atlas");
+
+        // GET
         app.get('/api/alertes', async (req, res) => {
             const resultat = await alertesCol.find().toArray();
             res.json(resultat);
         });
 
-        app.listen(3000, () => {
-            console.log("Servidor a http://localhost:3000");
+        // POST
+        app.post('/api/alertes', async (req, res) => {
+            const resultat = await alertesCol.insertOne(req.body);
+            res.status(201).json(resultat);
         });
 
-    } catch (err) {
-        console.error("Error connectant a Mongo:", err);
+        // PUT
+        app.put('/api/alertes/:id', async (req, res) => {
+            await alertesCol.updateOne(
+                { _id: new ObjectId(req.params.id) },
+                { $set: req.body }
+            );
+            res.json({ missatge: "Actualitzat" });
+        });
+
+        // DELETE
+        app.delete('/api/alertes/:id', async (req, res) => {
+            await alertesCol.deleteOne(
+                { _id: new ObjectId(req.params.id) }
+            );
+            res.json({ missatge: "Eliminat" });
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Servidor a http://localhost:${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error(error);
     }
 }
 
